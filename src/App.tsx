f83591310cd8ed156d733e9af2de5ededaabc468
satryaputra <muhammadsatrya12@gmail.com>
2023-11-30 09:39:26 +0700

    fix: mergin jonatan

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/templates/Layout/Layout";
-import { 
-  DashboardPage, 
-  ListVehiclesPage, 
-  ProfilePage, 
-  HomePage, 
+import {
+  DashboardPage,
+  ListVehiclesPage,
+  ProfilePage,
+  HomePage,
   LoginPage,
-  ForgotPasswordRequestPage, 
+  ForgotPasswordRequestPage,
 } from "@/pages";
 import { PrivateRoute } from "./components/templates";
 
@@ -15,11 +15,16 @@ export default function App() {
     <Routes>
       <Route path="/home" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/forgot-password/request" element={<ForgotPasswordRequestPage />} />
-      <Route element={<Layout />}>
-        <Route path="/" element={<DashboardPage />} />
-        <Route path="/vehicles" element={<ListVehiclesPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+      <Route
+        path="/forgot-password/request"
+        element={<ForgotPasswordRequestPage />}
+      />
+      <Route element={<PrivateRoute />}>
+        <Route element={<Layout />}>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/vehicles" element={<ListVehiclesPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Route>
       </Route>
     </Routes>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,9 +20,11 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
