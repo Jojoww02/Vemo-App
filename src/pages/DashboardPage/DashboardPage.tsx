d181b99acc046dc82e2d4a3eb7a58d5e1b5f8d6f
satryaputra <muhammadsatrya12@gmail.com>
2023-12-07 07:54:44 +0700

    improve conditional rendering vehicle card

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -6,48 +6,82 @@ import { useQuery } from "@tanstack/react-query";
 import { getVehiclesByUserIdFn } from "@/api/services/vehicle";
 import { IUserResponse, IVehicleResponse } from "@/api/types";
 import { BadgeAlert } from "lucide-react";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { VEHICLE_LIST } from "@/lib/constants/routes";
 
 export default function DashboardPage(): JSX.Element {
   const { data: user } = useQuery({ queryKey: ["me"] });
-  const [showTwoCard, setShowTwoCard] = useState(true);
 
   const navigate = useNavigate();
 
   const { data: vehicles, isSuccess } = useQuery({
     queryKey: ["vehicles"],
-    queryFn: async () => await getVehiclesByUserIdFn((user as IUserResponse).userId),
+    queryFn: async () =>
+      await getVehiclesByUserIdFn((user as IUserResponse).userId),
   });
-
-  const handleOnClick = () => {
-    setShowTwoCard(false);
-    navigate(VEHICLE_LIST);
-  };
 
   return (
     <>
       <div className="relative p-4 md:p-7 md:px-10 rounded-2xl shadow-[0px_0px_7px_#00000040] bg-[#898989]">
         <div className="hidden md:block absolute right-2 top-2">
           <Tooltip text="View All Vehicles">
-            <IconCircleArrowUpRightFilled size={27} className="text-white hover:scale-125 duration-500" type="button" onClick={handleOnClick} />
+            <IconCircleArrowUpRightFilled
+              size={27}
+              className="text-white hover:scale-125 duration-500"
+              type="button"
+            />
           </Tooltip>
         </div>
-        <div className={`flex flex-col xs:px-8 md:px-0 md:flex-row items-center gap-4 md:gap-6 ${vehicles?.length === 1 ? "w-1/2" : "w-full"}`}>
-          {isSuccess && vehicles.length > 0 ? (
-            (vehicles as IVehicleResponse[]).slice(0, showTwoCard ? 2 : vehicles.length).map((vehicle: IVehicleResponse) => <VehicleCard vehicleData={vehicle} key={vehicle.id} />)
-          ) : (
-            <div className="w-full flex flex-col items-center text-center gap-4">
-              <BadgeAlert className="text-white" size={50} />
-              <h1 className="text-white sm:text-xl">Opps! anda belum mendaftarkan kendaraan anda, daftar dulu yuk!</h1>
-            </div>
-          )}
+        <div
+          className={`flex flex-col xs:px-8 md:px-0 md:flex-row items-center gap-4 md:gap-6`}
+        >
+          {isSuccess &&
+            (vehicles.length > 0 ? (
+              vehicles.length > 2 ? (
+                (vehicles as IVehicleResponse[])
+                  .slice(0, 2)
+                  .map((vehicle: IVehicleResponse) => (
+                    <VehicleCard vehicleData={vehicle} key={vehicle.id} />
+                  ))
+              ) : (
+                <>
+                  {(vehicles as IVehicleResponse[])
+                    .slice(0, 1)
+                    .map((vehicle: IVehicleResponse) => (
+                      <VehicleCard vehicleData={vehicle} key={vehicle.id} />
+                    ))}
+                  {/* Vehicle Pending Card  */}
+                  <VehicleCard
+                    vehicleData={{
+                      id: "1",
+                      name: "string",
+                      ownerName: "string",
+                      licenseNumber: "string",
+                      type: "manual",
+                      condition: 50,
+                      purchasingDate: "120403",
+                    }}
+                  />
+                </>
+              )
+            ) : (
+              <div className="w-full flex flex-col items-center text-center gap-4">
+                <BadgeAlert className="text-white" size={50} />
+                <h1 className="text-white sm:text-xl">
+                  Opps! anda belum mendaftarkan kendaraan anda, daftar dulu yuk!
+                </h1>
+              </div>
+            ))}
         </div>
       </div>
       <div className="flex flex-col md:flex-row items-center w-full mt-5 gap-5 md:gap-10 mb-10">
         {featuresData.map((feature, index) => (
-          <FeatureCard image={feature.image} title={feature.title} key={index} path={feature.path} />
+          <FeatureCard
+            image={feature.image}
+            title={feature.title}
+            key={index}
+            path={feature.path}
+          />
         ))}
       </div>
     </>
