3b2b8a28ed6eaefa9fa3aa342c41b0a98564463b
satryaputra <muhammadsatrya12@gmail.com>
2023-12-07 14:26:51 +0700

    fix: index mobile

--- src/mobile/index.ts
+++ src/mobile/index.ts
@@ -4,5 +4,4 @@ export { default as LoginPageMobile } from "./LoginPageMobile";
 export { default as ForgotPasswordRequestPageMobile } from "./ForgotPasswordRequestPageMobile";
 export { default as ProfilePageMobile } from "./ProfilePageMobile";
 export { default as RegisterVehiclePageMobile } from "./RegisterVehiclePageMobile";
-export { default as RequestVehiclePagemobile } from "./RequestVehiclePagemobile";
 export { default as VehicleDetailPageMobile } from "./VehicleDetailPageMobile";
