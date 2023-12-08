78f733df908e2c28f1bf9eb739aecc9bf9d9780f
Maulvifatur82 <moulvi07papua@gmail.com>
2023-12-08 10:02:18 +0700

    (add) logic pending

--- src/components/molecules/VehicleCard/VehicleCard.tsx
+++ src/components/molecules/VehicleCard/VehicleCard.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Info } from "lucide-react";
+import { Info, Timer } from "lucide-react";
 import { Tooltip, VehicleCondition, VehicleIcon } from "@/components/atoms";
 import { REQUEST_MAINTENANCE_VEHICLE_PAGE, VEHICLE_DETAILS_PAGE } from "@/lib/constants/routes";
 import { IVehicleResponse } from "@/api/types";
@@ -9,29 +9,24 @@ interface Props {
 }
 
 export default function VehicleCard({ vehicleData }: Props): JSX.Element {
-  return (
+  
+  console.log("vehicledata", vehicleData);
+  return vehicleData.status === "approved" ? (
     <div className="flex px-4 md:py-2 gap-6 w-full lg:px-8 bg-white rounded-xl shadow-[0px_3px_7px_5px_#00000040]">
       <div className="self-center">
         <VehicleIcon type={vehicleData.type} />
       </div>
       <div className="pt-4 pb-3 w-full">
-        <h1 className="font-semibold text-sm xs:text-base lg:text-xl md:text-2xl tracking-wide sm:mb-1 md:mb-0">
-          {vehicleData.name}
-        </h1>
-        <h3 className="font-semibold text-[.6rem] xs:text-xs md:text-lg lg:text-lg lg:pb-3 text-[#898989]">
-          {vehicleData.licenseNumber}
-        </h3>
+        <h1 className="font-semibold text-sm xs:text-base lg:text-xl md:text-2xl tracking-wide sm:mb-1 md:mb-0">{vehicleData.name}</h1>
+        <h3 className="font-semibold text-[.6rem] xs:text-xs md:text-lg lg:text-lg lg:pb-3 text-[#898989]">{vehicleData.licenseNumber}</h3>
         <div className="w-full flex justify-between items-center gap-2">
           <div>
             <VehicleCondition condition={vehicleData.condition} />
           </div>
-          {window.location.pathname !== (REQUEST_MAINTENANCE_VEHICLE_PAGE) && (
+          {window.location.pathname !== REQUEST_MAINTENANCE_VEHICLE_PAGE && (
             <div className="lg:pr-5">
               <Tooltip text="View details vehicle">
-                <Link
-                  to={VEHICLE_DETAILS_PAGE(vehicleData.id)}
-                  className="flex items-center justify-center px-2 py-1 text-center bg-[#F4B400] text-white rounded-lg scale-75 xs:scale-90 lg:scale-110"
-                >
+                <Link to={VEHICLE_DETAILS_PAGE(vehicleData.id)} className="flex items-center justify-center px-2 py-1 text-center bg-[#F4B400] text-white rounded-lg scale-75 xs:scale-90 lg:scale-110">
                   <Info size={18} className="mr-1" />
                   <p className="text-xs">Detail</p>
                 </Link>
@@ -40,6 +35,11 @@ export default function VehicleCard({ vehicleData }: Props): JSX.Element {
           )}
         </div>
       </div>
+    </div>
+  ) : (
+    <div className="w-full flex-col text-center border-dashed rounded-2xl border-[3px] h-24 sm:h-28 md:h-[7.8rem] lg:h-[8.5rem] flex justify-center items-center text-white text-xs xs:text-base lg:text-xl">
+      <Timer className="text-white" size={35} />
+      <h1 className="text-white xs:text-base lg:text-xl xl:text-2xl w-4/5">Opps! anda belum mendaftarkan kendaraan anda, daftar dulu yuk!</h1>
     </div>
   );
 }
