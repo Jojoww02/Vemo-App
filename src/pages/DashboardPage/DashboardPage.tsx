c047858f383958d17d321c98136ba84dc02d94ee
Maulvifatur82 <moulvi07papua@gmail.com>
2023-12-09 23:50:45 +0700

    (add) rounded dashboardPage

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -49,7 +49,7 @@ export default function DashboardPage(): JSX.Element {
                       <VehicleCard vehicleData={vehicle} key={vehicle.id} />
                     ))}
                   {/* Vehicle Pending Card  */}
-                  <div className="w-full border-dashed border-[3px] h-24 sm:h-28 md:h-[7.8rem] lg:h-32 flex justify-center items-center ">
+                  <div className="w-full border-dashed rounded-2xl border-[3px] h-24 sm:h-28 md:h-[7.8rem] lg:h-32 flex justify-center items-center ">
                     <p className="text-white text-xs xs:text-base lg:text-lg  px-4">Daftarkan kendaraan kedua anda</p>
                   </div>
                 </>
