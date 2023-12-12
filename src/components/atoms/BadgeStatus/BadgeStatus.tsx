21335dd51be52c92805e59371287f43b0dddcec8
Maulvifatur82 <moulvi07papua@gmail.com>
2023-12-12 19:11:54 +0700

    (add) badge validation condition

--- src/components/atoms/BadgeStatus/BadgeStatus.tsx
+++ src/components/atoms/BadgeStatus/BadgeStatus.tsx
@@ -1,14 +1,18 @@
 import { Badge } from "@/components/ui/badge";
+import { VehicleCondition } from "@/lib/types";
+import { getVehicleCondition } from "@/lib/utils/vehicle";
 
 
 interface BadgeProps {
     condition: number
   }
 
-export default function BadgeStatus({ condition }: BadgeProps) {
+export default function BadgeStatus({ condition }: BadgeProps):JSX.Element {
+  const { description }: VehicleCondition =
+    getVehicleCondition(condition);
   return (
     <div>
-      <Badge className={`${condition <= 30 ? "bg-red-400" : condition <= 60 ? "bg-yellow-400" : "bg-green-400"}`}>Badge</Badge>
+      <Badge className={`${condition <= 30 ? "bg-red-400" : condition <= 60 ? "bg-yellow-400" : "bg-green-400"}`}>{description}</Badge>
     </div>
   );
 }
