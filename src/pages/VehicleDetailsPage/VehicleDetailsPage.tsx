ac7e539f2c0534104aaac639fd667baa8abf8fd2
Maulvifatur82 <moulvi07papua@gmail.com>
2023-12-12 18:04:37 +0700

    (add) badge

--- src/pages/VehicleDetailsPage/VehicleDetailsPage.tsx
+++ src/pages/VehicleDetailsPage/VehicleDetailsPage.tsx
@@ -8,6 +8,8 @@ import { id } from "date-fns/locale";
 import { useNavigate, useParams } from "react-router-dom";
 import { Button, VehicleIcon } from "@/components/atoms";
 import { VEHICLE_PARTS_PAGE } from "@/lib/constants/routes";
+import DetailVehicleCard from "@/components/molecules/DetailVehicleCard";
+import { componentsData } from "@/lib/data";
 
 export default function VehicleDetailsPage() {
   const { vehicleId } = useParams();
@@ -54,102 +56,79 @@ export default function VehicleDetailsPage() {
   return (
     <>
       {/* {isMobileResponsive ? ( */}
-        <div className="w-full flex gap-5 flex-col lg:flex-row">
-          <div className="lg:w-1/2 bg-[#F7F8F9] rounded-xl py-5 px-5 h-full min-h-[660px]">
-            <div className="w-full flex flex-col justify-center items-center">
-              <VehicleIcon type={isSuccess ? (vehicle as IVehicleResponse).type : "matic"} />
-              <h1 className="font-semibold text-xl lg:text-2xl mt-4 devide mb-5">{isSuccess && (vehicle as IVehicleResponse).name}</h1>
-            </div>
-            <div>
-              <Tabs defaultValue="account" className="w-full flex flex-col justify-center">
-                <TabsList className="w-full h-11 text-dark bg-gray-200">
-                  <TabsTrigger value="account" className="w-full h-full data-[state=active]:bg-primary/80 data-[state=active]:text-white">
-                    Information
-                  </TabsTrigger>
-                  <TabsTrigger value="password" className="w-full h-full data-[state=active]:bg-primary/80 data-[state=active]:text-white">
-                    History Service
-                  </TabsTrigger>
-                </TabsList>
-                <TabsContent value="account">
-                  <div className="text-start mt-5 sm:text-2xl flex flex-col gap-6">
-                    <h1 className="font-semibold text-lg sm:text-2xl">Motorcycle Information</h1>
-                    <div className="flex flex-col gap-2">
-                      <div className="flex gap-2">
-                        <IconUser />
-                        <h4 className="text-lg lg:text-xl">Owner Name :</h4>
-                      </div>
-                      <div className="text-lg">{isSuccess && (vehicle as IVehicleResponse).ownerName}</div>
-                    </div>
-                    <div className="flex flex-col gap-2">
-                      <div className="flex gap-2">
-                        <IconBike />
-                        <h4 className="text-lg lg:text-xl">Vehicle Name And Type :</h4>
-                      </div>
-                      <div className="text-lg">
-                        {isSuccess && (vehicle as IVehicleResponse).name} | {isSuccess && (vehicle as IVehicleResponse).type}
-                      </div>
-                    </div>
-                    <div className="flex flex-col gap-2">
-                      <div className="flex gap-2">
-                        <IconCalendarEvent />
-                        <h4 className="text-lg lg:text-xl">Date Vehicle Purchase :</h4>
-                      </div>
-                      <div className="text-lg">{isSuccess && format(new Date((vehicle as IVehicleResponse).purchasingDate), "dd MMMM yyyy", { locale: id })}</div>
-                    </div>
-                  </div>
-                </TabsContent>
-                <TabsContent value="password">
-                  {" "}
-                  <div className="text-start pl-6 mt-5">
-                    <div className="flex flex-col">
-                      <h1 className="list-disc text-xl pt-5 p font-bold sm:text-2xl">Riwayat Service</h1>
-                      <ul className="list-disc text-base pt-5 px-2 font-light sm:text-lg">
-                        <li className="border-b-2 w-[90%] sm:w-[60%]">
-                          20 Januari 2023 -{" "}
-                          <span className="text-primary cursor-pointer">
-                            See Details
-                          </span>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </TabsContent>
-              </Tabs>
-            </div>
+      <div className="w-full flex gap-5 flex-col lg:flex-row">
+        <div className="lg:w-1/2 bg-[#F7F8F9] rounded-xl py-5 px-5 h-full min-h-[660px]">
+          <div className="w-full flex flex-col justify-center items-center">
+            <VehicleIcon type={isSuccess ? (vehicle as IVehicleResponse).type : "matic"} />
+            <h1 className="font-semibold text-xl lg:text-2xl mt-4 devide mb-5">{isSuccess && (vehicle as IVehicleResponse).name}</h1>
           </div>
-          <div className="lg:w-1/2 lg:bg-[#F7F8F9] rounded-xl py-5 px-5 h-full min-h-[660px]">
-            <h1 className="text-[#898989] py-2 px-4 text-lg">Condition Part :</h1>
-            <div className="w-full px-2 flex mt-7 mb-10 flex-col gap-5 justify-center h-[28rem]  overflow-y-auto">
-              <div className="flex gap-4 mt-9">
-                <img src={"/machine.svg"} alt="" className="w-16 lg:w-24" />
-                <p className="font-bold text-xl">Mesin Motor</p>
-              </div>
-              <div className="flex gap-4">
-                <img src={"/oil.svg"} alt="" className="w-16 lg:w-24" />
-                <p className="font-bold text-xl">Oli Motor</p>
-              </div>
-              <div className="flex gap-4">
-                <img src={"/busi.svg"} alt="" className="w-16 lg:w-24" />
-                <p className="font-bold text-xl">Busi Motor</p>
-              </div>
-              <div className="flex gap-4">
-                <img src={"/aki.svg"} alt="" className="w-16 lg:w-24" />
-                <p className="font-bold text-xl">Aki Motor</p>
-              </div>
-              <div className="flex gap-4">
-                <img src={"/chain.svg"} alt="" className="w-16 lg:w-24" />
-                <p className="font-bold text-xl">Rantai Motor</p>
-              </div>
-              <div className="flex gap-4">
-                <img src={"/rem.svg"} alt="" className="w-16 lg:w-24" />
-                <p className="font-bold text-xl">Rem Motor</p>
-              </div>
-            </div>
-            <div className="w-full flex flex-col justify-center mb-10 ">
-              <Button onClick={() => navigate(VEHICLE_PARTS_PAGE)}>Request Perawatan</Button>
-            </div>
+          <div>
+            <Tabs defaultValue="account" className="w-full flex flex-col justify-center">
+              <TabsList className="w-full h-11 text-dark bg-gray-200">
+                <TabsTrigger value="account" className="w-full h-full data-[state=active]:bg-primary/80 data-[state=active]:text-white">
+                  Information
+                </TabsTrigger>
+                <TabsTrigger value="password" className="w-full h-full data-[state=active]:bg-primary/80 data-[state=active]:text-white">
+                  History Service
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="account">
+                <div className="text-start mt-5 sm:text-2xl flex flex-col gap-6">
+                  <h1 className="font-semibold text-lg sm:text-2xl">Motorcycle Information</h1>
+                  <div className="flex flex-col gap-2">
+                    <div className="flex gap-2">
+                      <IconUser />
+                      <h4 className="text-lg lg:text-xl">Owner Name :</h4>
+                    </div>
+                    <div className="text-lg">{isSuccess && (vehicle as IVehicleResponse).ownerName}</div>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <div className="flex gap-2">
+                      <IconBike />
+                      <h4 className="text-lg lg:text-xl">Vehicle Name And Type :</h4>
+                    </div>
+                    <div className="text-lg">
+                      {isSuccess && (vehicle as IVehicleResponse).name} | {isSuccess && (vehicle as IVehicleResponse).type}
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <div className="flex gap-2">
+                      <IconCalendarEvent />
+                      <h4 className="text-lg lg:text-xl">Date Vehicle Purchase :</h4>
+                    </div>
+                    <div className="text-lg">{isSuccess && format(new Date((vehicle as IVehicleResponse).purchasingDate), "dd MMMM yyyy", { locale: id })}</div>
+                  </div>
+                </div>
+              </TabsContent>
+              <TabsContent value="password">
+                <div className="text-start pl-6 mt-5">
+                  <div className="flex flex-col">
+                    <h1 className="list-disc text-xl pt-5 p font-bold sm:text-2xl">Riwayat Service</h1>
+                    <ul className="list-disc text-base pt-5 px-2 font-light sm:text-lg">
+                      <li className="border-b-2 w-[90%] sm:w-[60%]">
+                        20 Januari 2023 - <span className="text-primary cursor-pointer">See Details</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </TabsContent>
+            </Tabs>
           </div>
         </div>
+        <div className="lg:w-1/2 lg:bg-[#F7F8F9] rounded-xl py-5 px-5 h-full min-h-[660px]">
+          <h1 className="text-[#898989] py-2 px-4 text-lg">Condition Part :</h1>
+          <div className="w-full px-2 flex mt-7 mb-10 flex-col gap-5 justify-center h-[28rem]  overflow-y-auto">
+            <div className="flex flex-col gap-4 mt-14 px-4">
+              {componentsData.map((component, index) => (
+                <DetailVehicleCard key={index} title={component?.name} condition={component?.condition} image={component?.name} />
+              ))}
+            </div>
+          </div>
+          <div className="w-full flex flex-col justify-center mb-10 ">
+            <Button onClick={() => navigate(VEHICLE_PARTS_PAGE)}>Request Perawatan</Button>
+          </div>
+        </div>
+      </div>
       {/* ) : (
         <VehicleDetailPageMobile />
        )} */}
