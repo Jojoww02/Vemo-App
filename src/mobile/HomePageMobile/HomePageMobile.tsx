ad46e10ade25d19d40e428bfe1b9637618924f44
satryaputra <muhammadsatrya12@gmail.com>
2023-12-14 08:54:02 +0700

    update: relatve button to every mobile device

--- src/mobile/HomePageMobile/HomePageMobile.tsx
+++ src/mobile/HomePageMobile/HomePageMobile.tsx
@@ -1,36 +1,49 @@
-import { Button } from "@/components/atoms";
 import { Link } from "react-router-dom";
+import { Button } from "@/components/atoms";
+import { LOGIN_PAGE, REGISTER_USER_PAGE } from "@/lib/constants/routes";
 import LandingImage from "../../assets/homeLanding/landing-image.webp";
 import GradientWhiteMobile from "../../assets/homeLanding/linear-gradient-white-mobile.svg";
-import { LOGIN_PAGE, REGISTER_USER_PAGE } from "@/lib/constants/routes";
+
 export default function HomePageMobile(): JSX.Element {
   return (
-    <div className="w-full h-screen relative">
+    <div className="w-full h-screen relative overflow-y-hidden">
       <div className="relative top-0 sm:hidden">
         <img src={LandingImage} alt="" className="w-full" />
-        <img src={GradientWhiteMobile} alt="" className="w-full absolute -bottom-1" />
       </div>
-
       <div className="flex flex-col items-center sm:grid sm:place-items-center text-dark text-center sm:bg-[url('/src/assets/homeLanding/landing-image.webp')] sm:bg-cover sm:h-full sm:relative">
         <div className="hidden sm:block sm:absolute sm:w-full sm:h-screen sm:top-0 sm:left-0 sm:bg-black sm:bg-opacity-30"></div>
         <div className="w-full absolute bottom-0">
-          <h1 className="text-5xl font-bold mt-5 sm:hidden">Welcome!</h1>
-          <p className="text-xl font-medium my-2 sm:text-2xl sm:hidden">to</p>
-          <div className="flex items-center justify-center gap-2 mb-12 sm:hidden">
-            <img src="/src/assets/iconVemo.svg" alt="" className="w-[15%]" />
-            <h3 className="text-2xl font-semibold text-primary italic sm:text-3xl">VEMO</h3>
+          <div>
+            <img
+              src={GradientWhiteMobile}
+              alt=""
+              className="w-full sm:hidden"
+            />
           </div>
-          <div className="sm:hidden w-full px-10 flex flex-col gap-3 mb-5">
-            <Button asChild className="py-6 text-lg font-semibold">
-              <Link to={REGISTER_USER_PAGE} className="w-full">
-                Register
-              </Link>
-            </Button>
-            <Button asChild className="py-6 text-lg font-semibold bg-dark hover:bg-dark/80">
-              <Link to={LOGIN_PAGE} className="w-full">
-                Log In
-              </Link>
-            </Button>
+          <div className="w-full bg-white py-8 -mt-1 sm:hidden">
+            <h1 className="text-5xl font-bold sm:hidden">Welcome!</h1>
+            <p className="text-xl font-medium my-2 sm:text-2xl sm:hidden">to</p>
+            <div className="flex items-center justify-center gap-2 mb-12 sm:hidden">
+              <img src="/src/assets/iconVemo.svg" alt="" className="w-[15%]" />
+              <h3 className="text-2xl font-semibold text-primary italic sm:text-3xl">
+                VEMO
+              </h3>
+            </div>
+            <div className="sm:hidden w-full px-10 flex flex-col gap-3 mb-4">
+              <Button asChild className="py-6 text-lg font-semibold">
+                <Link to={REGISTER_USER_PAGE} className="w-full">
+                  Register
+                </Link>
+              </Button>
+              <Button
+                asChild
+                className="py-6 text-lg font-semibold bg-dark hover:bg-dark/80"
+              >
+                <Link to={LOGIN_PAGE} className="w-full">
+                  Log In
+                </Link>
+              </Button>
+            </div>
           </div>
         </div>
 
@@ -40,7 +53,9 @@ export default function HomePageMobile(): JSX.Element {
             <p className="text-xl font-medium my-2 sm:text-3xl">to</p>
             <div className="flex items-center justify-center mb-12">
               <img src="/src/assets/iconVemo.svg" alt="" className="w-[27%]" />
-              <h3 className="text-2xl font-semibold text-primary italic sm:text-3xl">VEMO</h3>
+              <h3 className="text-2xl font-semibold text-primary italic sm:text-3xl">
+                VEMO
+              </h3>
             </div>
             <div className="flex flex-col gap-3 mb-12">
               <Button asChild className="py-6 text-lg font-semibold">
@@ -48,7 +63,10 @@ export default function HomePageMobile(): JSX.Element {
                   Register
                 </Link>
               </Button>
-              <Button asChild className="py-6 text-lg font-semibold bg-dark hover:bg-dark/80">
+              <Button
+                asChild
+                className="py-6 text-lg font-semibold bg-dark hover:bg-dark/80"
+              >
                 <Link to={LOGIN_PAGE} className="w-full">
                   Log In
                 </Link>
