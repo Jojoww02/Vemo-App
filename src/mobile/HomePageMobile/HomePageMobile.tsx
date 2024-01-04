d13be53e74605cf03f1e103476fa1ddad9eca04b
satryaputra <muhammadsatrya12@gmail.com>
2024-01-04 20:48:06 +0700

    update path icon HomePage

--- src/mobile/HomePageMobile/HomePageMobile.tsx
+++ src/mobile/HomePageMobile/HomePageMobile.tsx
@@ -52,7 +52,7 @@ export default function HomePageMobile(): JSX.Element {
             <h1 className="text-5xl font-bold mt-5">Selamat Datang!</h1>
             <p className="text-xl font-medium my-2 sm:text-3xl">di</p>
             <div className="flex items-center justify-center mb-12">
-              <img src="/src/assets/iconVemo.svg" alt="" className="w-16" />
+              <img src="/iconVemo.svg" alt="" className="w-16" />
               <h3 className="text-2xl font-semibold text-primary italic sm:text-3xl ml-2">
                 VEMO
               </h3>
