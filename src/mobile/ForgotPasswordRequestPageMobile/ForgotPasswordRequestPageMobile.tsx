cd9f723f81c00c56c3d0a9fb2ad2511c7a88618e
Maulvifatur82 <moulvi07papua@gmail.com>
2024-01-04 19:17:41 +0700

    (add) handle forgotPassword

--- src/mobile/ForgotPasswordRequestPageMobile/ForgotPasswordRequestPageMobile.tsx
+++ src/mobile/ForgotPasswordRequestPageMobile/ForgotPasswordRequestPageMobile.tsx
@@ -1,11 +1,12 @@
 import zod from "zod";
-import { Link } from "react-router-dom";
-import { LOGIN_PAGE } from "@/lib/constants/routes";
+import { useNavigate } from "react-router-dom";
 import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, Input } from "@/components/atoms";
+import { AlertForm, Button, Input } from "@/components/atoms";
 import { isObjectEmpty } from "@/lib/utils/common";
 import useMutateAuth from "@/hooks/useMutateAuth";
+import IconArrow from "../../assets/notification/Icon-arrow.svg";
+import { LOGIN_PAGE } from "@/lib/constants/routes";
 
 const ForgotPasswordRequestSchema = zod.object({
   email: zod.string().min(1, "Email diperlukan").email("Email tidak ditemukan"),
@@ -14,6 +15,7 @@ const ForgotPasswordRequestSchema = zod.object({
 export type ForgotPasswordRequestInput = zod.TypeOf<typeof ForgotPasswordRequestSchema>;
 
 export default function ForgotPasswordRequestPageMobile(): JSX.Element {
+  const navigate = useNavigate();
   const methods = useForm<ForgotPasswordRequestInput>({
     resolver: zodResolver(ForgotPasswordRequestSchema),
   });
@@ -24,27 +26,25 @@ export default function ForgotPasswordRequestPageMobile(): JSX.Element {
     await forgotPasswordRequest.mutateAsync(email);
   };
 
+  forgotPasswordRequest.isSuccess && navigate(LOGIN_PAGE);
+
   return (
     <div className="pt-8 px-6 h-screen">
+      <div onClick={() => navigate(-1)}>
+        <img src={IconArrow} alt="" className="py-5 w-5 lg:w-7 " />
+      </div>
       <h1 className="font-bold text-3xl sm:text-4xl">Lupa Password?</h1>
       <p className="pt-3 sm:text-lg sm:w-3/4">Jangan khawatir! masukkan alamat email yang tertaut dengan akun anda.</p>
       <div className="mt-10 flex flex-col">
         <FormProvider {...methods}>
           <form onSubmit={methods.handleSubmit(onSubmitHandler)} className="flex-col w-full flex gap-5">
+            {forgotPasswordRequest.isError && <AlertForm title={(forgotPasswordRequest.error as any).response.data.message} description={(forgotPasswordRequest.error as any).response.data.errors} />}
             <Input name="email" label="Email" isFill={methods.watch().email} placeholder="Masukkan Email Anda" type="email" />
-            <Button className="flex mt-7 items-center justify-center py-6 text-lg font-semibold" type="submit" disabled={!isObjectEmpty(methods.formState.errors)}>
+            <Button className="flex mt-7 items-center justify-center py-6 text-lg font-semibold" type="submit" disabled={!isObjectEmpty(methods.formState.errors)} isLoading={forgotPasswordRequest.isPending}>
               Kirim Kode
             </Button>
           </form>
         </FormProvider>
-      </div>
-      <div className="flex justify-center text-center mt-10 font-medium  gap-1 tracking-wide sm:text-lg">
-        <p className="">Ingat password?</p>
-        <div className="mb-10">
-          <Link to={LOGIN_PAGE} className="text-[#0587BE]">
-            <p>Masuk</p>
-          </Link>
-        </div>
       </div>
     </div>
   );
--- src/pages/ForgotPasswordRequestPage/ForgotPasswordRequestPage.tsx
+++ src/pages/ForgotPasswordRequestPage/ForgotPasswordRequestPage.tsx
@@ -1,12 +1,13 @@
 import zod from "zod";
-import { Button, Input } from "@/components/atoms";
+import { AlertForm, Button, Input } from "@/components/atoms";
 import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { isObjectEmpty } from "@/lib/utils/common";
 import useMobile from "@/hooks/useMobile";
 import { ForgotPasswordRequestPageMobile } from "@/mobile";
 import useMutateAuth from "@/hooks/useMutateAuth";
+import { LOGIN_PAGE } from "@/lib/constants/routes";
 
 const ForgotPasswordRequestSchema = zod.object({
   email: zod.string().min(1, "Email diperlukan").email("Email tidak ditemukan"),
@@ -14,6 +15,7 @@ const ForgotPasswordRequestSchema = zod.object({
 
 export type ForgotPasswordRequestInput = zod.TypeOf<typeof ForgotPasswordRequestSchema>;
 export default function ForgotPasswordRequestPage(): JSX.Element {
+  const navigate = useNavigate();
   const methods = useForm<ForgotPasswordRequestInput>({
     resolver: zodResolver(ForgotPasswordRequestSchema),
   });
@@ -24,6 +26,8 @@ export default function ForgotPasswordRequestPage(): JSX.Element {
     await forgotPasswordRequest.mutateAsync(email);
   };
 
+  forgotPasswordRequest.isSuccess && navigate(LOGIN_PAGE);
+
   const isMobileResponive = useMobile();
   return (
     <>
@@ -32,23 +36,18 @@ export default function ForgotPasswordRequestPage(): JSX.Element {
           <div className="absolute left-0 top-0 w-full h-full bg-black bg-opacity-30"></div>
 
           {/* Card */}
-          <div className="w-[35%] xl:w-[30%] h-[80%] 2xl:h-[85%] p-8 rounded-[2rem] bg-white z-10 shadow-[0px_2px_7px_5px_#00000040]">
+          <div className="w-[35%] xl:w-[30%] p-8 rounded-[2rem] bg-white z-10 shadow-[0px_2px_7px_5px_#00000040]">
             <p className="font-bold text-dark text-lg xl:text-3xl text-center">Lupa Password?</p>
             <p className="mt-3 font-medium text-[#8391A1] text-md text-center">Jangan khawatir! masukkan alamat email yang tertaut dengan akun anda.</p>
             <FormProvider {...methods}>
-              <form onSubmit={methods.handleSubmit(onSubmitHandler)} className="flex flex-col mt-10 gap-32 xl:gap-40">
+              <form onSubmit={methods.handleSubmit(onSubmitHandler)} className="flex flex-col mt-10 gap-4 mb-10">
+                {forgotPasswordRequest.isError && <AlertForm title={(forgotPasswordRequest.error as any).response.data.message} description={(forgotPasswordRequest.error as any).response.data.errors} />}
                 <Input name="email" label="Email" isFill={methods.watch().email} placeholder="Masukkan Email Anda" type="email" />
-                <Button className="py-6 text-lg font-semibold" disabled={!isObjectEmpty(methods.formState.errors)} type="submit">
+                <Button className="py-6 text-lg font-semibold" disabled={!isObjectEmpty(methods.formState.errors)} isLoading={forgotPasswordRequest.isPending} type="submit">
                   Kirim Kode
                 </Button>
               </form>
             </FormProvider>
-            <p className=" font-medium text-center mt-6">
-              Ingat Password?
-              <Link to={"/login"}>
-                <span className="text-[#0586BE] ml-1 font-semibold cursor-pointer">Masuk</span>
-              </Link>
-            </p>
           </div>
         </div>
       ) : (
